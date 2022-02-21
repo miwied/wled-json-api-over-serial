88ae827ecc384b96887c3fd306c28b8f8041fdfd
miwied <derpixed@gmail.com>
2022-02-21 23:49:20 +0100

    changed few things

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
-#include <Arduino.h>
+//this code sends simple control-json packets to a esp8266 with wled
+#include "Arduino.h"
 #include "ArduinoJson.h"
-#include <SoftwareSerial.h>
+#include "SoftwareSerial.h"
 
 SoftwareSerial wledOut(2, 4); //rx, tx
 
@@ -13,11 +14,10 @@ void sendJson(int color) //16-Bit color value
   int g = ((((color >> 5) & 0x3F) * 259) + 33) >> 6;
   int b = (((color & 0x1F) * 527) + 23) >> 6;
 
-  Serial.println(String(r) + ", " + String(g) + ", " + String(b));
   StaticJsonDocument<192> doc;
 
   doc["on"] = true;
-  doc["bri"] = 255;
+  doc["bri"] = 100;
   doc["ps"] = -1;
 
   JsonObject seg_0 = doc["seg"].createNestedObject();
@@ -27,17 +27,21 @@ void sendJson(int color) //16-Bit color value
   seg_0_col_0.add(g);
   seg_0_col_0.add(b);
   seg_0["fx"] = 0;
+
   serializeJson(doc, wledOut);
 
   digitalWrite(LED_BUILTIN, LOW);
   delay(10);
 }
 
+
+
 void setup() 
 {
   Serial.begin(115200);
-  wledOut.begin(115200);
+  wledOut.begin(921600);
   
+  pinMode(15, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
 }
@@ -45,13 +49,19 @@ void setup()
 
 void loop() 
 {
+  if(digitalRead(15)==LOW)
+  {
+    sendJson(13875); //türkis
+    delay(250);
+  }
+  delay(1000);
   sendJson(13875); //türkis
-  delay(500);
+  delay(5000);
   sendJson(39156); //lila
-  delay(500);
+  delay(5000);
   sendJson(63488); //rot
-  delay(500);
+  delay(5000);
   sendJson(65535); //weiss
-  delay(500);
+  delay(5000);
 }
 
