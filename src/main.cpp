e1b582e6026948ac2d414ec28c5921b8057db10b
miwied <derpixed@gmail.com>
2022-02-23 22:21:13 +0100

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@ void randomColor()
   seg_0_col_0.add(random(0, 254));
   seg_0_col_0.add(random(0, 254));
 
-  serializeJson(doc, Serial); //print json to Serial for debug 
+  serializeJson(doc, Serial); //print json to Serial for debugging
   serializeJson(doc, wledOut);
   Serial.println("");
 }
@@ -43,7 +43,7 @@ void sendRGB(int r, int g, int b)
   seg_0_col_0.add(g);
   seg_0_col_0.add(b);
 
-  serializeJson(doc, Serial); //print json to Serial for debug 
+  serializeJson(doc, Serial); //print json to Serial for debugging 
   serializeJson(doc, wledOut);
   Serial.println("");
   delay(10);
