883c59d16a20501972f072d035cde4b1a7ac38e5
miwied <90411228+miwied@users.noreply.github.com>
2022-03-08 21:09:56 +0100

    updated comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /*
-This code is just for testing the json api over serial.
-Sending µC is a esp32 and recieving µC is a esp8266 with a custom compiled wled (WLED-0.13.0-b6) with a serial baudrate of 115200.
+This code is just for testing the wled json api over serial.
+Sending-µC is a esp32 and recieving-µC is a esp8266 with a custom compiled wled (WLED-0.13.0-b6) with a serial baudrate of 115200.
 
 connections:
 esp32 pin 2(RX) <-> esp8266 pin 1(TX)
@@ -12,26 +12,6 @@ esp32 gnd <-> esp8266 gnd
 #include "ArduinoJson.h"
 
 HardwareSerial wledSerial(1);
-
-void randomColor() 
-{
-  StaticJsonDocument<192> doc;
-
-  doc["on"] = true;
-  doc["bri"] = 110;
-
-  JsonArray seg_0_col_0 = doc["seg"][0]["col"].createNestedArray();
-  seg_0_col_0.add(random(0, 254));
-  seg_0_col_0.add(random(0, 254));
-  seg_0_col_0.add(random(0, 254));
-
-  for(int i = 0; i<2; i++)
-  {
-  serializeJson(doc, wledSerial);
-  wledSerial.println('\n');
-  }
-}
-
 
 void jsonRequest()
 {
@@ -59,7 +39,7 @@ void setup()
   Serial.begin(115200); //serial monitor
 
   //the baudrate of 115200 worked perfectly for me
-  wledSerial.begin(115200,SERIAL_8N1, 2, 4); //tested baudrates: 115200, 230400, 460800, 500000,  921600, 1000000, 1500000
+  wledSerial.begin(115200, SERIAL_8N1, 2, 4); //tested baudrates: 115200, 230400, 460800, 500000, 921600, 1000000, 1500000
 
   pinMode(15, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -68,7 +48,7 @@ void setup()
 
 void loop() 
 {
-
+  //change color on button press
   if(digitalRead(15)==LOW)
   {
     sendRGB(random(0, 254), random(0, 254), random(0, 254));
